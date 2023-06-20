6a27b2fbed42e9e9a6059333abcd62ae03475c64
Chillios <chillios@duck.com>
2023-06-20 15:01:19 +0200

    chore: fix trezor typing for getAddress

--- internal/config/index.js
+++ internal/config/index.js
@@ -12,7 +12,7 @@ const baseConfig = (name) => {
     /**
      * bright blue bold italic underline
      */
-    console.log('\x1b[1m\x1b[34m\x1b[3m\x1b[4m%s\x1b[0m', 'Vite Analyze Enabled');
+    console.info('\x1b[1m\x1b[34m\x1b[3m\x1b[4m%s\x1b[0m', 'Vite Analyze Enabled');
 
     rollupPlugins.push(visualizer({
       emitFile: true,
