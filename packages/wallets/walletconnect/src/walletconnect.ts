fc3b94dab81f805c3171dee284fee3065354b66b
0xGeneral <ildepadronhosna@gmail.com>
2023-06-23 19:09:08 +0200

    Move walletconnect back to sign API

--- packages/wallets/walletconnect/src/walletconnect.ts
+++ packages/wallets/walletconnect/src/walletconnect.ts
@@ -1,86 +1,99 @@
-import { AVAXToolbox, ETHToolbox, getProvider } from '@thorswap-lib/toolbox-evm';
+import { hexlify } from '@ethersproject/bytes';
+import { toUtf8Bytes } from '@ethersproject/strings';
+import { ETHToolbox, getProvider } from '@thorswap-lib/toolbox-evm';
 import { Chain, WalletOption } from '@thorswap-lib/types';
 import QRCodeModal from '@walletconnect/qrcode-modal';
-import Client, { type SignClient } from '@walletconnect/sign-client';
-import { SessionTypes, SignClientTypes } from '@walletconnect/types';
+import Client from '@walletconnect/sign-client';
+import type { SessionTypes, SignClientTypes } from '@walletconnect/types';
 
 import {
   DEFAULT_APP_METADATA,
+  DEFAULT_EIP155_METHODS,
   DEFAULT_LOGGER,
   DEFAULT_RELAY_URL,
   WC_SUPPORTED_CHAINS,
+  ETHEREUM_MAINNET_ID,
 } from './constants.js';
-import { getEVMSigner } from './evmSigner.js';
-import { chainToChainId } from './helpers.js';
+import { chainToChainId, getAddressByChain } from './helpers.js';
 import { getRequiredNamespaces } from './namespaces.js';
 
-// const getToolbox = async ({
-//   chain,
-//   ethplorerApiKey,
-//   covalentApiKey,
-//   address,
-//   walletconnectClient,
-//   session,
-// }: {
-//   // @ts-ignore
-//   walletconnectClient: Client;
-//   session: SessionTypes.Struct;
-//   chain: (typeof SUPPORTED_CHAINS)[number];
-//   covalentApiKey?: string;
-//   ethplorerApiKey?: string;
-//   address: string;
-// }) => {
-//   const from = address;
+const SUPPORTED_CHAINS = [Chain.Binance, Chain.Ethereum, Chain.THORChain] as const;
 
-//   switch (chain) {
-//     case Chain.Avalanche:
-//     case Chain.BinanceSmartChain:
-//     case Chain.Ethereum: {
-//       const provider = getProvider(chain);
+const getToolbox = async ({
+  chain,
+  ethplorerApiKey,
+  address,
+  walletconnectClient,
+  session,
+}: {
+  // @ts-ignore
+  walletconnectClient: Client;
+  session: SessionTypes.Struct;
+  chain: (typeof SUPPORTED_CHAINS)[number];
+  ethplorerApiKey?: string;
+  address: string;
+}) => {
+  const from = address;
 
-//       const signer = {
-//         getAddress: async () => address,
-//         _isSigner: true,
-//         sendTransaction: async (tx: EIP1559TxParams) => {
-//           const txHash: string = await walletconnectClient.request({
-//             chainId: chainToChainId(chain),
-//             topic: session.topic,
-//             request: {
-//               method: DEFAULT_EIP155_METHODS.ETH_SEND_TRANSACTION,
-//               params: [
-//                 {
-//                   from,
-//                   to: tx.to?.toLocaleLowerCase(),
-//                   data: tx.data || undefined,
-//                   value: BigNumber.from(tx.value || 0).toHexString(),
-//                 },
-//               ],
-//             },
-//           });
-//           return txHash;
-//         },
-//       } as unknown as Signer;
+  switch (chain) {
+    case Chain.Ethereum: {
+      if (!ethplorerApiKey) throw new Error('Ethplorer API key not found');
 
-//       return toolbox;
-//     }
-//     default:
-//       throw new Error('Chain is not supported');
-//   }
-// };
+      const provider = getProvider(chain);
+
+      const toolbox = ETHToolbox({
+        provider,
+        ethplorerApiKey,
+      });
+
+      const transfer = async (params: any) => {
+        const txAmount = params.amount.amount().toHexString();
+        const gasLimit = (
+          await toolbox.estimateGasLimit({
+            asset: params.asset,
+            recipient: params.recipient,
+            amount: params.amount,
+            memo: params.memo,
+          })
+        ).toHexString();
+        const gasPrice = (await toolbox.estimateGasPrices()).fast.maxFeePerGas?.toHexString();
+        const txHash: string = await walletconnectClient.request({
+          chainId: ETHEREUM_MAINNET_ID,
+          topic: session.topic,
+          request: {
+            method: DEFAULT_EIP155_METHODS.ETH_SEND_TRANSACTION,
+            params: [
+              {
+                from,
+                to: params.recipient,
+                data: params.memo ? hexlify(toUtf8Bytes(params.memo)) : '0x',
+                gasPrice,
+                gasLimit,
+                value: txAmount,
+              },
+            ],
+          },
+        });
+        return txHash;
+      };
+
+      return { ...toolbox, transfer };
+    }
+    default:
+      throw new Error('Chain is not supported');
+  }
+};
 
 const getWalletconnect = async (
   chains: Chain[],
-  walletConnectProjectId?: string,
+  walletConnectProjectId: string,
   walletconnectOptions?: SignClientTypes.Options,
-): Promise<{
-  session: SessionTypes.Struct;
-  accounts: string[];
-  client: InstanceType<typeof SignClient>;
-}> => {
+) => {
   try {
     const requiredNamespaces = getRequiredNamespaces(chains.map(chainToChainId));
-    //@ts-ignore
-    const client = await (Client as typeof Client.SignClient).init({
+
+    // @ts-ignore
+    const client = await Client.init({
       logger: DEFAULT_LOGGER,
       relayUrl: DEFAULT_RELAY_URL,
       projectId: walletConnectProjectId,
@@ -95,38 +108,35 @@ const getWalletconnect = async (
     // Open QRCode modal if a URI was returned (i.e. we're not connecting an existing pairing).
     if (uri) {
       // @ts-ignore
-      (QRCodeModal as typeof QRCodeModal.default).open(uri, () => {
+      QRCodeModal.open(uri, () => {
         console.log('EVENT', 'QR Code Modal closed');
       });
     }
 
     const session = await approval();
-    const accounts = Object.values<SessionTypes.Namespace>(session.namespaces)
-      .map((namespace) => namespace.accounts)
+    const accounts = Object.values(session.namespaces)
+      .map((namespace: any) => namespace.accounts)
       .flat();
 
-    //@ts-ignore
     return { session, accounts, client };
   } catch (e) {
-    throw new Error('Unable to establish connection through walletconnect');
+    console.error(e);
   } finally {
     // @ts-ignore
-    (QRCodeModal as typeof QRCodeModal.default).close();
+    QRCodeModal.close();
   }
+  return undefined;
 };
-
-export type Walletconnect = Awaited<ReturnType<typeof getWalletconnect>>;
 
 const connectWalletconnect =
   ({
     addChain,
-    config: { ethplorerApiKey, walletConnectProjectId, covalentApiKey },
+    config: { ethplorerApiKey, walletConnectProjectId },
   }: {
     addChain: any;
     config: {
-      covalentApiKey?: string;
       ethplorerApiKey?: string;
-      walletConnectProjectId?: string;
+      walletConnectProjectId: string;
     };
   }) =>
   async (
@@ -140,35 +150,21 @@ const connectWalletconnect =
       walletconnectOptions,
     );
 
-    const promises = chainsToConnect.map(async (chain) => {
-      if (
-        chain === Chain.Avalanche ||
-        chain === Chain.BinanceSmartChain ||
-        chain === Chain.Ethereum
-      ) {
-        // TODO improve check if keys are set
-        if (chain === Chain.Ethereum && !ethplorerApiKey)
-          throw new Error('Ethplorer API key not found');
-        if (chain !== Chain.Ethereum && !covalentApiKey)
-          throw new Error('Covalent API key not found');
+    if (!walletconnect) throw new Error('Unable to establish connection through walletconnect');
 
-        // TODO(@0xGeneral): Check on typings
-        const provider = getProvider(chain) as unknown as any;
-        const signer = await getEVMSigner({ walletconnect, chain, provider });
-        const toolbox =
-          chain === Chain.Ethereum && ethplorerApiKey
-            ? ETHToolbox({
-                provider,
-                signer,
-                ethplorerApiKey,
-              })
-            : covalentApiKey
-            ? AVAXToolbox({
-                provider,
-                signer,
-                covalentApiKey,
-              })
-            : undefined;
+    const { session, accounts, client } = walletconnect;
+
+    const promises = chainsToConnect.map(async (chain) => {
+      const address = getAddressByChain(chain, accounts);
+      const getAddress = () => address;
+
+      const toolbox = await getToolbox({
+        walletconnectClient: client,
+        session,
+        address,
+        chain,
+        ethplorerApiKey,
+      });
 
         const address = await signer.getAddress();
 
