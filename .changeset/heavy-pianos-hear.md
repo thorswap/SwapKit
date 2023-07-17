d6ae5a195c08db29831392a3dcd1ab1fd0da37e3
General <ildepadronhosna@gmail.com>
2023-07-17 13:10:46 +0200

    chore: remove unused walletconnect web3 provider (#201)

---
+++ .changeset/heavy-pianos-hear.md
@@ -0,0 +1,5 @@
+---
+'@thorswap-lib/walletconnect': minor
+---
+
+Remove unused @walletconnect/web3-provider
--- packages/wallets/walletconnect/src/index.ts
+++ packages/wallets/walletconnect/src/index.ts
@@ -1,4 +1,3 @@
 export * from './constants.js';
 export * from './types.js';
 export * from './walletconnect.js';
-export { default as WalletConnectProvider } from '@walletconnect/web3-provider';
