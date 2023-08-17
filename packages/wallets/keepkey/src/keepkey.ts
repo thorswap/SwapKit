e4cf8340727166792b224e2b08ca50008f090e55
bithighlander <bithighlander@gmail.com>
2023-08-16 21:13:58 -0500

    untested but complete

--- packages/wallets/keepkey/src/keepkey.ts
+++ packages/wallets/keepkey/src/keepkey.ts
@@ -1,9 +1,11 @@
 import { Signer } from '@ethersproject/abstract-signer';
 import { AVAXToolbox, BSCToolbox, ETHToolbox, getProvider } from '@thorswap-lib/toolbox-evm';
-import { BCHToolbox, BTCToolbox, DOGEToolbox, LTCToolbox } from '@thorswap-lib/toolbox-utxo';
-import { Chain, ConnectWalletParams, DerivationPathArray, WalletOption } from '@thorswap-lib/types';
-
+import { BCHToolbox, BTCToolbox, DOGEToolbox, LTCToolbox, UTXOTransferParams } from '@thorswap-lib/toolbox-utxo';
+import { Chain, ConnectWalletParams, DerivationPathArray, WalletOption, UTXO, FeeOption } from '@thorswap-lib/types';
+import { KeepKeySdk } from "@keepkey/keepkey-sdk";
 import { getEVMSigner } from './signer/evm.js';
+import { Psbt } from 'bitcoinjs-lib';
+import { toCashAddress } from 'bchaddrjs';
 
 export const KEEPKEY_SUPPORTED_CHAINS = [
   Chain.Avalanche,
@@ -40,7 +42,7 @@ const getToolbox = async (params: Params) => {
   switch (chain) {
     case Chain.BinanceSmartChain:
     case Chain.Avalanche:
-    case Chain.Ethereum:
+    case Chain.Ethereum: {
       const provider = getProvider(chain, rpcUrl);
       const signer = (await getEVMSigner({ sdk, chain, derivationPath, provider })) as Signer;
       const address = await signer.getAddress();
@@ -52,20 +54,21 @@ const getToolbox = async (params: Params) => {
 
       const evmParams = { api, signer, provider };
       const walletMethods =
-        chain === Chain.Ethereum
-          ? ETHToolbox({ ...evmParams, ethplorerApiKey: ethplorerApiKey as string })
-          : (chain === Chain.Avalanche ? AVAXToolbox : BSCToolbox)({
-              ...evmParams,
-              covalentApiKey: covalentApiKey as string,
-            });
+          chain === Chain.Ethereum
+              ? ETHToolbox({ ...evmParams, ethplorerApiKey: ethplorerApiKey as string })
+              : (chain === Chain.Avalanche ? AVAXToolbox : BSCToolbox)({
+                ...evmParams,
+                covalentApiKey: covalentApiKey as string,
+              });
 
-      return { address, walletMethods: { ...walletMethods, getAddress: () => address } };
-
+      return { address, walletMethods: { ...walletMethods, getAddress: () => address } };  
+    }
     case Chain.Bitcoin:
     case Chain.BitcoinCash:
     case Chain.Dogecoin:
     case Chain.Litecoin:
       if (!utxoApiKey && !api) throw new Error('UTXO API key not found');
+      const coin = chain.toLowerCase() as 'btc' | 'bch' | 'ltc' | 'doge';
 
       const scriptType =
         derivationPath[0] === 84
@@ -92,28 +95,104 @@ const getToolbox = async (params: Params) => {
       // Placeholder functions
       const getAddress = async function(){
         try{
-
           //Unsigned TX
-          // let addressInfo = {
-          //   address_n: [2147483732, 2147483648, 2147483648, 0, 0],
-          //   script_type: 'p2wpkh',
-          //   coin:'Bitcoin'
-          // }
-          // let response = await sdk.address.utxoGetAddress({
-          //   address_n: addressInfo.addressNList,
-          //   script_type:addressInfo.scriptType,
-          //   coin:addressInfo.coin
-          // })
+          let addressInfo = {
+            address_n: [2147483732, 2147483648, 2147483648, 0, 0],
+            script_type: 'p2wpkh',
+            coin:'Bitcoin'
+          }
+          let response = await sdk.address.utxoGetAddress(addressInfo)
           return "address"
         }catch(e){
-          
+          console.error(e)
         }
       }
-      const signTransaction = async () => 'PlaceholderSignedTransaction';
-      const transfer = async () => 'PlaceholderTransfer';
+      const address = await getAddress();
+      const signTransaction = async (psbt: Psbt, inputs: UTXO[], memo: string = '') => {
+        const address_n = derivationPath.map((pathElement, index) =>
+            index < 3 ? (pathElement | 0x80000000) >>> 0 : pathElement,
+        );
+
+        
+        let outputs = psbt.txOutputs.map((output: any) => {
+          const outputAddress =
+              chain === Chain.BitcoinCash && output.address
+                  ? toCashAddress(output.address)
+                  : output.address;
+
+          // Strip prefix from BCH address to compare with stripped address from Trezor
+          const isChangeAddress =
+              chain === Chain.BitcoinCash && outputAddress
+                  ? (toolbox as ReturnType<typeof BCHToolbox>).stripPrefix(outputAddress) === address
+                  : outputAddress === address;
+
+          // OP_RETURN
+          if (!output.address) {
+            return {
+              // @ts-ignore
+              op_return_data: Buffer.from(memo).toString('hex'),
+              amount: '0',
+              script_type: 'PAYTOOPRETURN',
+            };
+          }
+
+          // Change Address
+          if (isChangeAddress) {
+            return {
+              address_n,
+              amount: output.value,
+              script_type: scriptType.output,
+            };
+          }
+
+          // Outgoing UTXO
+          return {
+            address: outputAddress,
+            amount: output.value,
+            script_type: 'PAYTOADDRESS',
+          };
+          
+        });
+        
+        let txToSign = {
+          coin: "Bitcoin",
+          inputs,
+          outputs,
+          version: 1,
+          locktime: 0,
+        }
+        let responseSign = await sdk.utxo.utxoSignTransaction(txToSign);
+
+        return responseSign.serialized
+      };
+
+      const transfer = async ({
+                                from,
+                                recipient,
+                                feeOptionKey,
+                                feeRate,
+                                memo,
+                                ...rest
+                              }: UTXOTransferParams) => {
+        if (!from) throw new Error('From address must be provided');
+        if (!recipient) throw new Error('Recipient address must be provided');
+
+        const { psbt, inputs } = await toolbox.buildTx({
+          ...rest,
+          memo,
+          feeOptionKey,
+          recipient,
+          feeRate: feeRate || (await toolbox.getFeeRates())[feeOptionKey || FeeOption.Fast],
+          sender: from,
+          fetchTxHex: chain === Chain.Dogecoin,
+        });
+
+        const txHex = await signTransaction(psbt, inputs, memo);
+        return toolbox.broadcastTx(txHex);
+      };
 
       return {
-        address: 'PlaceholderAddress',
+        address,
         walletMethods: { ...utxoMethods, getAddress, signTransaction, transfer },
       };
 
@@ -131,7 +210,23 @@ const connectKeepKey =
   }: ConnectWalletParams) =>
   async (chain: (typeof KEEPKEY_SUPPORTED_CHAINS)[number], derivationPath: DerivationPathArray) => {
 
+    const spec = "http://localhost:1646/spec/swagger.json";
+    const apiKey = localStorage.getItem("apiKey") || "1234";
+    const config = {
+      apiKey,
+      pairingInfo: {
+        name: "KeepKey-template Demo App",
+        imageUrl: "https://pioneers.dev/coins/keepkey.png",
+        basePath: spec,
+        url: "http://localhost:1646",
+      },
+    };
+    // init
+    const sdk = await KeepKeySdk.create(config);
+    
+
     const { address, walletMethods } = await getToolbox({
+      sdk,
       api: apis[chain as Chain.Ethereum],
       rpcUrl: rpcUrls[chain],
       chain,
