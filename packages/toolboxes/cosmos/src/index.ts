f53c91e37dde7270cf1d2ebe793f9d7f60facc06
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2023-10-23 21:43:52 +0200

    Update index.ts

--- packages/toolboxes/cosmos/src/index.ts
+++ packages/toolboxes/cosmos/src/index.ts
@@ -3,5 +3,6 @@ export * from './thorchainUtils/index.ts';
 export { BinanceToolbox } from './toolbox/binance.ts';
 export { GaiaToolbox } from './toolbox/gaia.ts';
 export { MayaToolbox, ThorchainToolbox } from './toolbox/thorchain.ts';
+export { KujiraToolbox } from './toolbox/kujira.ts';
 export * from './types.ts';
 export * from './util.ts';
