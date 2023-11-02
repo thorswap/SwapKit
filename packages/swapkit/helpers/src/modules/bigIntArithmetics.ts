d4e1ff16d405051792e066777d768347db26951d
towanTG <namelessthorguard@gmail.com>
2023-11-02 13:10:37 +0200

    chore: adds to fixed and improves fromBigint

--- packages/swapkit/helpers/src/modules/bigIntArithmetics.ts
+++ packages/swapkit/helpers/src/modules/bigIntArithmetics.ts
@@ -7,7 +7,7 @@ type NumberPrimitivesType = {
   number: number;
   string: string;
 };
-type NumberPrimitives = bigint | number | string;
+export type NumberPrimitives = bigint | number | string;
 type InitialisationValueType = NumberPrimitives | BigIntArithmetics | SwapKitNumber;
 
 type SKBigIntParams = InitialisationValueType | { decimal?: number; value: number | string };
