6ce024f8fc5a01f29a233ae47a6a9997d46e7e63
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2023-11-06 16:48:31 +0100

    Update constants.ts

--- packages/wallets/wc/src/constants.ts
+++ packages/wallets/wc/src/constants.ts
@@ -9,6 +9,9 @@ export const BSC_MAINNET_ID = 'eip155:56';
 export const AVALACHE_MAINNET_ID = 'eip155:43114';
 export const THORCHAIN_MAINNET_ID = 'cosmos:thorchain-mainnet-v1';
 export const BINANCE_MAINNET_ID = 'cosmos:Binance-Chain-Tigris';
+export const COSMOS_HUB_MAINNET_ID = 'cosmos:cosmoshub-4'; // not supported by Wallet Connect
+export const KUJIRA_MAINNET_ID = 'cosmos:kaiyo-1';
+export const MAYACHAIN_MAINNET_ID = 'cosmos:mayachain-mainnet-v1'
 export const ARBITRUM_ONE_MAINNET_ID = 'eip155:42161';
 export const OPTIMISM_MAINNET_ID = 'eip155:10';
 export const POLYGON_MAINNET_ID = 'eip155:137';
@@ -84,7 +87,7 @@ export enum DEFAULT_NEAR_METHODS {
 export enum DEFAULT_NEAR_EVENTS {}
 
 export const WC_SUPPORTED_CHAINS = [
-  Chain.Binance,
+  Chain.Binance, // Not supported by Wallet Connect
   Chain.BinanceSmartChain,
   Chain.Ethereum,
   Chain.THORChain,
@@ -92,4 +95,7 @@ export const WC_SUPPORTED_CHAINS = [
   Chain.Arbitrum,
   Chain.Optimism,
   Chain.Polygon,
+  Chain.Cosmos,
+  Chain.Maya,
+  Chain.Kujira,
 ] as const;
