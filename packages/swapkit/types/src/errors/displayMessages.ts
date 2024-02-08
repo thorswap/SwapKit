fc098fa8f1d17219156ed852002f1f2be331a3a5
Oleg Petrov <oleg.petrov.eth@gmail.com>
2024-02-08 17:31:57 -0500

    add swap amount too low

--- packages/swapkit/types/src/errors/displayMessages.ts
+++ packages/swapkit/types/src/errors/displayMessages.ts
@@ -18,7 +18,7 @@ export const ERROR_DISPLAY_MESSAGES: Record<string, string> = {
   [ERROR_CODE.MISSING_SOURCE_ADDRESS_FOR_SYNTH]: 'Source address is required for synth quote.',
   [ERROR_CODE.AFF_ADDRESS_AND_BPS_OR_NEITHER]:
     'Must provide affiliateAddress and affiliateBasisPoints params, or neither.',
-  [ERROR_CODE.AFF_ADDRESS_TOO_LONG]: 'affiliateAddress too long: 3 characters max.',
+  [ERROR_CODE.AFF_ADDRESS_TOO_LONG]: 'affiliateAddress too long: 4 characters max.',
   [ERROR_CODE.AFF_BPS_INTEGER_MAX_500]:
     'affiliateBasisPoints must be an integer between 0 and 100.',
   [ERROR_CODE.SOURCE_ADDRESS_INVALID_FOR_SELL_CHAIN]: 'Source address {0} invalid for sell chain.',
@@ -26,6 +26,7 @@ export const ERROR_DISPLAY_MESSAGES: Record<string, string> = {
     'Destination address {0} invalid for buy chain.',
   [ERROR_CODE.PREFERRED_PROVIDER_NOT_SUPPORTED]: 'Preferred provider not supported.',
   [ERROR_CODE.SOURCE_ADDRESS_SMART_CONTRACT]: 'Source address is a smart contract.',
+  [ERROR_CODE.SWAP_AMOUNT_TOO_LOW]: 'Swap amount too low for this pair.',
   [ERROR_CODE.DESTINATION_ADDRESS_SMART_CONTRACT]: 'Destination address is a smart contract.',
   [ERROR_CODE.BUY_AMOUNT_MUST_BE_POSITIVE_INTEGER]: 'Buy amount must be a positive integer.',
   [ERROR_CODE.INVALID_PROVIDER]: 'Invalid provider {0}.',
