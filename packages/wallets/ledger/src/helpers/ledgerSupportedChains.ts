e916619424ef09d478da74940acad7a496212065
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-03-17 15:41:44 +0100

    Update ledgerSupportedChains.ts

--- packages/wallets/ledger/src/helpers/ledgerSupportedChains.ts
+++ packages/wallets/ledger/src/helpers/ledgerSupportedChains.ts
@@ -1,14 +1,18 @@
 import { Chain } from "@swapkit/types";
 
 export const LEDGER_SUPPORTED_CHAINS = [
+  Chain.Arbitrum,
   Chain.Avalanche,
   Chain.Binance,
   Chain.BinanceSmartChain,
   Chain.Bitcoin,
   Chain.BitcoinCash,
   Chain.Cosmos,
+  Chain.Dash,
   Chain.Dogecoin,
   Chain.Ethereum,
   Chain.Litecoin,
+  Chain.Optimism,
+  Chain.Polygon,
   Chain.THORChain,
 ] as const;
