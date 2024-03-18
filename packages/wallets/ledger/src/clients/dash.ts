f1156648d3b4dca16c45ea319904ed299fe358ae
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-03-18 13:52:58 +0100

    Update dash.ts

--- packages/wallets/ledger/src/clients/dash.ts
+++ packages/wallets/ledger/src/clients/dash.ts
@@ -3,15 +3,16 @@ import { getNetwork } from "@swapkit/toolbox-utxo";
 import type { DerivationPathArray } from "@swapkit/types";
 import { Chain, NetworkDerivationPath } from "@swapkit/types";
 
-import { getWalletFormatFor } from "../helpers/derivationPath.ts";
 import { UTXOLedgerInterface } from "../interfaces/LedgerInterfaces.ts";
 
 export class DashLedger extends UTXOLedgerInterface {
   constructor(derivationPath: DerivationPathArray = NetworkDerivationPath.DASH) {
     super();
+    this.additionalSignParams = { additionals: [], segwit: false, useTrustedInputForSegwit: false };
     this.addressNetwork = getNetwork(Chain.Dash);
     this.chain = "dash";
+    this.walletFormat = "legacy";
+
     this.derivationPath = derivationPathToString(derivationPath);
-    this.walletFormat = getWalletFormatFor(this.derivationPath) as "legacy" | "bech32" | "p2sh";
   }
 }
