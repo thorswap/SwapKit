ed58b58d4b86ffda80dd1a9afee51d0f054b27e3
Oleg Petrov <90647190+olegpetroveth@users.noreply.github.com>
2024-04-04 10:29:19 -0400

    Bump chainflip 1.3 (#683)
    * manually update chainflip tokenlist
    
    * bump cf sdk
    
    * pnpm install
    
    ---------
    
    Co-authored-by: Chillios <17621507+chillios-dev@users.noreply.github.com>

--- packages/swapkit/tokens/src/tokenLists/Chainflip.ts
+++ packages/swapkit/tokens/src/tokenLists/Chainflip.ts
@@ -1,12 +1,31 @@
 export const list = {
-  name: "native-l1",
-  timestamp: "2023-10-17T00:00:00.000Z",
+  name: "Chainflip",
+  timestamp: "2024-02-27T04:35:02.814Z",
   version: { major: 1, minor: 0, patch: 0 },
-  keywords: ["Native"],
+  keywords: ["chainflip"],
   tokens: [
+    { chain: "BTC", identifier: "BTC.BTC", decimals: 8 },
     { chain: "DOT", identifier: "DOT.DOT", decimals: 10 },
-    { chain: "FLIP", identifier: "FLIP.FLIP", decimals: 18 },
+    { chain: "ETH", identifier: "ETH.ETH", decimals: 18 },
+    {
+      address: "0x826180541412d574cf1336d22c0c0a287822678a",
+      chain: "ETH",
+      identifier: "ETH.FLIP-0X826180541412D574CF1336D22C0C0A287822678A",
+      decimals: 18,
+    },
+    {
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      chain: "ETH",
+      identifier: "ETH.USDC-0XA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48",
+      decimals: 6,
+    },
+    {
+      address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+      chain: "ETH",
+      identifier: "ETH.USDT-0XDAC17F958D2EE523A2206206994597C13D831EC7",
+      decimals: 6,
+    },
   ],
-  count: 2,
-  logo: "https://static.thorswap.net/logo.png",
+  count: 6,
+  logo: "https://static.thorswap.net/token-list/images/flip.flip.png",
 } as const;
