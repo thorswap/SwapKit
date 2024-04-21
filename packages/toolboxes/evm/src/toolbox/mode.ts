1138b9d8f4b809f5a1f33f257107595355abd985
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2024-04-21 14:32:31 +0200

    Create mode.ts

---
+++ packages/toolboxes/evm/src/toolbox/mode.ts
@@ -0,0 +1,49 @@
+import { BaseDecimal, Chain, ChainId, ChainToExplorerUrl } from "@swapkit/helpers";
+import type { BrowserProvider, JsonRpcProvider, Signer } from "ethers";
+
+import type { CovalentApiType } from "../api/covalentApi.ts";
+import { covalentApi } from "../api/covalentApi.ts";
+import { getBalance } from "../index.ts";
+
+import { BaseEVMToolbox } from "./BaseEVMToolbox.ts";
+
+const getNetworkParams = () => ({
+  chainId: ChainId.Mode,
+  chainName: "Mode",
+  nativeCurrency: { name: "Ethereum", symbol: Chain.Ethereum, decimals: BaseDecimal.ETH },
+  // Use external rpc URL so wallets don't throw warning to user
+  rpcUrls: ["https://mainnet.mode.network"],
+  blockExplorerUrls: [ChainToExplorerUrl[Chain.Mode]],
+});
+
+export const MODEToolbox = ({
+  api,
+  provider,
+  signer,
+  covalentApiKey,
+}: {
+  api?: CovalentApiType;
+  covalentApiKey: string;
+  signer: Signer;
+  provider: JsonRpcProvider | BrowserProvider;
+}) => {
+  const modeApi = api || covalentApi({ apiKey: covalentApiKey, chainId: ChainId.Mode });
+  const baseToolbox = BaseEVMToolbox({ provider, signer });
+
+  return {
+    ...baseToolbox,
+    getNetworkParams,
+    getBalance: (
+      address: string,
+      potentialScamFilter = true,
+      overwriteProvider?: JsonRpcProvider | BrowserProvider,
+    ) =>
+      getBalance({
+        provider: overwriteProvider || provider,
+        api: modeApi,
+        address,
+        chain: Chain.Mode,
+        potentialScamFilter,
+      }),
+  };
+};
