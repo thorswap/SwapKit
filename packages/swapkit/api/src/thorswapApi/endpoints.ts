9d2bb635b34cec4c71967b4e1f80b4aa43fa3847
towan <towan@thorswap.finance>
2024-05-15 21:05:49 +0400

    feat: introduces zod to api

--- packages/swapkit/api/src/thorswapApi/endpoints.ts
+++ packages/swapkit/api/src/thorswapApi/endpoints.ts
@@ -1,22 +1,40 @@
-import { RequestClient, RequestClientWithErrors } from "@swapkit/helpers";
-import type {
-  BorrowParams,
-  BorrowResponse,
-  CachedPrice,
-  CachedPricesParams,
-  GasPriceInfo,
-  LendingAssetItem,
-  LoansParams,
-  LoansResponse,
-  QuoteParams,
-  QuoteResponse,
-  RepayParams,
-  RepayResponse,
-  TokenListProvidersResponse,
-  TxnResponse,
+import { RequestClient } from "@swapkit/helpers";
+import {
+  ApiV1ErrorSchema,
+  type BorrowParams,
+  type BorrowResponse,
+  type CachedPrice,
+  type CachedPricesParams,
+  type GasPriceInfo,
+  type LendingAssetItem,
+  type LoansParams,
+  type LoansResponse,
+  type QuoteParams,
+  type QuoteResponse,
+  type RepayParams,
+  type RepayResponse,
+  type TokenListProvidersResponse,
+  type TxnResponse,
 } from "./types.ts";
 
 const baseUrl = "https://api.thorswap.finance";
+
+const requestClient = RequestClient.extend({
+  hooks: {
+    afterResponse: [
+      async (_request, _options, response) => {
+        const body = await response.json();
+
+        try {
+          const errorBody = ApiV1ErrorSchema.parse(body);
+          return new Response(JSON.stringify(errorBody), { status: 200 });
+        } catch (_error) {
+          return response;
+        }
+      },
+    ],
+  },
+});
 
 export function getCachedPrices({ tokens, ...options }: CachedPricesParams) {
   const body = new URLSearchParams();
@@ -32,46 +50,46 @@ export function getCachedPrices({ tokens, ...options }: CachedPricesParams) {
   if (options.lookup) body.append("lookup", "true");
   if (options.sparkline) body.append("sparkline", "true");
 
-  return RequestClient.post<CachedPrice[]>(`${baseUrl}/tokenlist/cached-price`, {
+  return requestClient.post<CachedPrice[]>(`${baseUrl}/tokenlist/cached-price`, {
     body: body.toString(),
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
   });
 }
 
 export function getSwapQuote(searchParams: QuoteParams) {
-  return RequestClientWithErrors.get<QuoteResponse>(`${baseUrl}/aggregator/tokens/quote`, {
+  return requestClient.get<QuoteResponse>(`${baseUrl}/aggregator/tokens/quote`, {
     searchParams,
   });
 }
 
 export function getBorrowQuote(searchParams: BorrowParams) {
-  return RequestClientWithErrors.get<BorrowResponse>(`${baseUrl}/aggregator/lending/borrow`, {
+  return requestClient.get<BorrowResponse>(`${baseUrl}/aggregator/lending/borrow`, {
     searchParams,
   });
 }
 
 export function getRepayQuote(searchParams: RepayParams) {
-  return RequestClientWithErrors.get<RepayResponse>(`${baseUrl}/aggregator/lending/repay`, {
+  return requestClient.get<RepayResponse>(`${baseUrl}/aggregator/lending/repay`, {
     searchParams,
   });
 }
 
 export function getLendingAssets() {
-  return RequestClient.get<LendingAssetItem[]>(`${baseUrl}/aggregator/lending/assets`);
+  return requestClient.get<LendingAssetItem[]>(`${baseUrl}/aggregator/lending/assets`);
 }
 
 export function getLoans(searchParams: LoansParams) {
-  return RequestClient.get<LoansResponse>(`${baseUrl}/aggregator/lending/loans`, { searchParams });
+  return requestClient.get<LoansResponse>(`${baseUrl}/aggregator/lending/loans`, { searchParams });
 }
 
 export function getGasRates() {
-  return RequestClient.get<GasPriceInfo[]>(`${baseUrl}/resource-worker/gasPrice/getAll`);
+  return requestClient.get<GasPriceInfo[]>(`${baseUrl}/resource-worker/gasPrice/getAll`);
 }
 
 export function getTxnDetails(txHash: string) {
-  return RequestClient.get<TxnResponse>(`${baseUrl}/apiusage/v2/txn`, { searchParams: { txHash } });
+  return requestClient.get<TxnResponse>(`${baseUrl}/apiusage/v2/txn`, { searchParams: { txHash } });
 }
 
 export function getTokenListProviders() {
-  return RequestClient.get<TokenListProvidersResponse>(`${baseUrl}/tokenlist/providers`);
+  return requestClient.get<TokenListProvidersResponse>(`${baseUrl}/tokenlist/providers`);
 }
--- packages/swapkit/helpers/src/modules/requestClient.ts
+++ packages/swapkit/helpers/src/modules/requestClient.ts
@@ -1,9 +1,7 @@
-import type { Options } from "ky";
+import type { KyInstance, Options } from "ky";
 import ky from "ky";
-import type { ApiV1Error } from "../types/errors/apiV1.ts";
 
 let kyClient: typeof ky;
-let kyClientWithErrors: typeof ky;
 
 export const defaultRequestHeaders =
   typeof window !== "undefined"
@@ -17,59 +15,25 @@ export function setRequestClientConfig({ apiKey, ...config }: Options & { apiKey
   });
 }
 
-function isApiV1Error(error: unknown): error is ApiV1Error {
-  return (
-    (error as ApiV1Error).status !== undefined &&
-    (error as ApiV1Error).type !== undefined &&
-    (error as ApiV1Error).code !== undefined &&
-    (error as ApiV1Error).module !== undefined &&
-    (error as ApiV1Error).complete !== undefined &&
-    (error as ApiV1Error).identifier !== undefined &&
-    (error as ApiV1Error).message !== undefined
-  );
-}
-
 function getKyClient() {
   if (kyClient) return kyClient;
   kyClient = ky.create({ headers: defaultRequestHeaders });
   return kyClient;
 }
 
-function getKyClientWithErrors() {
-  if (kyClientWithErrors) return kyClientWithErrors;
-  kyClientWithErrors = ky.create({
-    headers: defaultRequestHeaders,
-    hooks: {
-      afterResponse: [
-        async (_request, _options, response) => {
-          const body = await response.json();
-
-          if (isApiV1Error(body)) {
-            return new Response(JSON.stringify(body), { status: 200 });
-          }
-
-          return new Response(body, { status: 200 });
-        },
-      ],
-    },
-  });
-  return kyClientWithErrors;
-}
+const getTypedBaseRequestClient = (kyClient: KyInstance) => ({
+  get: <T>(url: string | URL | Request, options?: Options) => kyClient.get(url, options).json<T>(),
+  post: <T>(url: string | URL | Request, options?: Options) =>
+    kyClient.post(url, options).json<T>(),
+});
 
 export const RequestClient = {
-  get: <T>(url: string | URL | Request, options?: Options) =>
-    getKyClient().get(url, options).json<T>(),
-  post: <T>(url: string | URL | Request, options?: Options) =>
-    getKyClient().post(url, options).json<T>(),
-};
-
-/**
- * This client returns known errors as JSON objects without throwing an error.
- * This means the HTTP code will be 200, and the response will be the error object.
- */
-export const RequestClientWithErrors = {
-  get: <T>(url: string | URL | Request, options?: Options) =>
-    getKyClientWithErrors().get(url, options).json<T>(),
-  post: <T>(url: string | URL | Request, options?: Options) =>
-    getKyClientWithErrors().post(url, options).json<T>(),
+  ...getTypedBaseRequestClient(getKyClient()),
+  extend: (options: Options) => {
+    const extendedClient = getKyClient().extend(options);
+    return {
+      ...getTypedBaseRequestClient(extendedClient),
+      extend: RequestClient.extend,
+    };
+  },
 };
