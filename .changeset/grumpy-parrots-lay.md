4784f4dff61d07208978cea73c6f66c5e667e570
towan <towan@thorswap.finance>
2024-06-06 16:13:27 +0400

    feat: fix token list action

---
+++ .changeset/grumpy-parrots-lay.md
@@ -0,0 +1,6 @@
+---
+"@swapkit/tokens": patch
+"@swapkit/api": patch
+---
+
+Fixes tokenlist and moves it to v2
--- packages/swapkit/tokens/src/tokenLists/Chainflip.ts
+++ packages/swapkit/tokens/src/tokenLists/Chainflip.ts
@@ -1,25 +1,52 @@
 export const list = {
-  name: "Chainflip",
-  timestamp: "2024-02-27T04:35:02.814Z",
+  provider: "CHAINFLIP",
+  name: "CHAINFLIP",
+  timestamp: "2024-05-29T16:00:28.730Z",
   version: { major: 1, minor: 0, patch: 0 },
-  keywords: ["chainflip"],
+  keywords: [],
+  count: 6,
   tokens: [
-    { chain: "BTC", identifier: "BTC.BTC", decimals: 8 },
-    { chain: "DOT", identifier: "DOT.DOT", decimals: 10 },
-    { chain: "ETH", identifier: "ETH.ETH", decimals: 18 },
     {
-      address: "0x826180541412d574cf1336d22c0c0a287822678a",
-      chain: "ETH",
-      identifier: "ETH.FLIP-0X826180541412D574CF1336D22C0C0A287822678A",
-      decimals: 18,
+      chain: "BTC",
+      identifier: "BTC.BTC",
+      decimals: 8,
+      logoURI: "https://storage.googleapis.com/token-list-swapkit-dev/images/btc.btc.png",
     },
     {
-      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      chain: "DOT",
+      identifier: "DOT.DOT",
+      decimals: 10,
+      logoURI: "https://storage.googleapis.com/token-list-swapkit-dev/images/dot.dot.png",
+    },
+    {
       chain: "ETH",
-      identifier: "ETH.USDC-0XA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48",
+      identifier: "ETH.ETH",
+      decimals: 18,
+      logoURI: "https://storage.googleapis.com/token-list-swapkit-dev/images/eth.eth.png",
+    },
+    {
+      address: "0x826180541412D574cf1336d22c0C0a287822678A",
+      chain: "ETH",
+      identifier: "ETH.FLIP-0x826180541412D574cf1336d22c0C0a287822678A",
+      decimals: 18,
+      logoURI:
+        "https://storage.googleapis.com/token-list-swapkit-dev/images/eth.flip-0x826180541412d574cf1336d22c0c0a287822678a.png",
+    },
+    {
+      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      chain: "ETH",
+      identifier: "ETH.USDC-0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
       decimals: 6,
+      logoURI:
+        "https://storage.googleapis.com/token-list-swapkit-dev/images/eth.usdc-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
+    },
+    {
+      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      chain: "ETH",
+      identifier: "ETH.USDT-0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      decimals: 6,
+      logoURI:
+        "https://storage.googleapis.com/token-list-swapkit-dev/images/eth.usdt-0xdac17f958d2ee523a2206206994597c13d831ec7.png",
     },
   ],
-  count: 5,
-  logo: "https://static.thorswap.net/token-list/images/flip.flip.png",
 } as const;
