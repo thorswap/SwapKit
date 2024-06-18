39e6ba550f96584a379e916c5a03ea58777c3d12
towan <towan@thorswap.finance>
2024-06-18 14:16:43 +0400

    fix: bsc call

--- packages/toolboxes/evm/src/toolbox/bsc.ts
+++ packages/toolboxes/evm/src/toolbox/bsc.ts
@@ -1,17 +1,10 @@
 import { BaseDecimal, Chain, ChainId, ChainToExplorerUrl } from "@swapkit/helpers";
-import type { BrowserProvider, JsonRpcProvider, Provider, Signer } from "ethers";
+import type { BrowserProvider, JsonRpcProvider, Signer } from "ethers";
 
 import type { CovalentApiType } from "../api/covalentApi.ts";
 import { covalentApi } from "../api/covalentApi.ts";
-import { type CallParams, getBalance } from "../index.ts";
-
-import type { WithSigner } from "./BaseEVMToolbox.ts";
-import {
-  BaseEVMToolbox,
-  createContract,
-  isBrowserProvider,
-  isStateChangingCall,
-} from "./BaseEVMToolbox.ts";
+import { getBalance } from "../index.ts";
+import { BaseEVMToolbox } from "./BaseEVMToolbox.ts";
 
 const getNetworkParams = () => ({
   chainId: ChainId.BinanceSmartChainHex,
@@ -35,68 +28,8 @@ export const BSCToolbox = ({
   const bscApi = api || covalentApi({ apiKey: covalentApiKey, chainId: ChainId.BinanceSmartChain });
   const baseToolbox = BaseEVMToolbox({ provider, signer, isEIP1559Compatible: false });
 
-  const call = async <T>(
-    provider: Provider,
-    {
-      callProvider,
-      signer,
-      contractAddress,
-      abi,
-      funcName,
-      funcParams = [],
-      txOverrides,
-    }: WithSigner<CallParams>,
-    // biome-ignore lint/complexity/noExcessiveCognitiveComplexity: TODO: refactor
-  ): Promise<T> => {
-    const contractProvider = callProvider || provider;
-    if (!contractAddress) throw new Error("contractAddress must be provided");
-
-    const isStateChanging = isStateChangingCall(abi, funcName);
-
-    const feeData = isStateChanging ? { ...(await baseToolbox.estimateGasPrices()).average } : {};
-
-    if (isStateChanging && isBrowserProvider(contractProvider) && signer) {
-      const txObject = await baseToolbox.createContractTxObject({
-        contractAddress,
-        abi,
-        funcName,
-        funcParams,
-        txOverrides: { ...feeData, ...txOverrides },
-      });
-
-      return baseToolbox.EIP1193SendTransaction(txObject) as Promise<T>;
-    }
-
-    const contract = createContract(contractAddress, abi, contractProvider);
-
-    // only use signer if the contract function is state changing
-    if (isStateChanging) {
-      if (!signer) throw new Error("Signer is not defined");
-
-      const address = txOverrides?.from || (await signer.getAddress());
-      if (!address) throw new Error("No signer address found");
-
-      const result = await contract.connect(signer).getFunction(funcName)(...funcParams, {
-        ...feeData,
-        ...txOverrides,
-        /**
-         * nonce must be set due to a possible bug with ethers.js,
-         * expecting a synchronous nonce while the JsonRpcProvider delivers Promise
-         */
-        nonce: txOverrides?.nonce || (await contractProvider.getTransactionCount(address)),
-      });
-
-      return typeof result === "string" ? result : result?.hash;
-    }
-
-    const result = await contract.getFunction(funcName)(...funcParams);
-
-    return typeof result === "string" ? result : result?.hash;
-  };
-
   return {
     ...baseToolbox,
-    call: <T>(params: CallParams) => call<T>(provider, { signer, ...params }),
     getNetworkParams,
     getBalance: (
       address: string,
