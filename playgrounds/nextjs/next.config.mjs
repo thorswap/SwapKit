41115db19ddd41a69e640e3237779336d1d64699
Chillios <17621507+chillios-dev@users.noreply.github.com>
2024-06-24 11:04:32 +0200

    playgrounds/nextjs: add packages, update next.config.js (#857)
    Co-authored-by: Tawnee <tawnee.a@proton.me>

--- playgrounds/nextjs/next.config.mjs
+++ playgrounds/nextjs/next.config.mjs
@@ -1,12 +1,40 @@
+import { createRequire } from "module";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
-      config.resolve.fallback = { fs: false, path: false };
+      config.plugins.push(new NodePolyfillPlugin());
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          global: [require.resolve("global"), "default"],
+          process: "process/browser",
+        }),
+      );
+
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          "global.crypto": JSON.stringify(require("crypto-browserify")),
+          "global.Uint8Array": JSON.stringify(Uint8Array),
+          "global.process": "process",
+        }),
+      );
+
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: require.resolve("crypto-browserify"),
+        process: require.resolve("process/browser"),
+      };
     }
 
     return {
