6ba87cf96bfa5a4da69511257ab9f81219c1a12b
Oleg Petrov <90647190+olegpetroveth@users.noreply.github.com>
2024-06-27 07:57:13 -0400

    Add LP endpoints to Microgard API (#872)
    * add raw full member endpoint
    
    * Add LP endpoints to SDK
    
    * chore: moves some code
    
    * chore: renaming
    
    ---------
    
    Co-authored-by: towan <towan@thorswap.finance>

--- packages/swapkit/api/src/microgard/endpoints.ts
+++ packages/swapkit/api/src/microgard/endpoints.ts
@@ -1,7 +1,8 @@
-import { RequestClient } from "@swapkit/helpers";
-import type { PoolDetail, PoolPeriod, THORNameDetails } from "./types.ts";
+import { AssetValue, RequestClient, SwapKitNumber } from "@swapkit/helpers";
+import type { LiquidityPositionRaw, PoolDetail, PoolPeriod, THORNameDetails } from "./types.ts";
 
 const baseUrl = "https://mu.thorswap.net";
+const midgardUrl = "https://midgard.thorswap.net";
 
 export function getTHORNameDetails(thorname: string) {
   return RequestClient.get<THORNameDetails>(`${baseUrl}/thorname/lookup/${thorname}`);
@@ -17,4 +18,28 @@ export function getTHORNamesByAddress(address: string) {
 
 export function getTHORChainPools(period: PoolPeriod) {
   return RequestClient.get<PoolDetail[]>(`${baseUrl}/pools`, { searchParams: { period } });
+}
+
+export function getLiquidityPositionsRaw(addresses: string[]) {
+  return RequestClient.get<LiquidityPositionRaw[]>(
+    `${midgardUrl}/v2/full_member?address=${addresses.join(",")}`,
+  );
+}
+
+export async function getLiquidityPositions(addresses: string[]) {
+  const rawLiquidityPositions = await getLiquidityPositionsRaw(addresses);
+
+  return rawLiquidityPositions.map((rawPosition) => ({
+    assetRegisteredAddress: rawPosition.assetAddress,
+    asset: AssetValue.fromStringWithBaseSync(rawPosition.pool, rawPosition.assetAdded),
+    assetPending: AssetValue.fromStringWithBaseSync(rawPosition.pool, rawPosition.assetPending),
+    assetWithdrawn: AssetValue.fromStringWithBaseSync(rawPosition.pool, rawPosition.assetWithdrawn),
+    runeRegisteredAddress: rawPosition.runeAddress,
+    rune: AssetValue.fromStringWithBaseSync("THOR.RUNE", rawPosition.runeAdded),
+    runePending: AssetValue.fromStringWithBaseSync("THOR.RUNE", rawPosition.runePending),
+    runeWithdrawn: AssetValue.fromStringWithBaseSync("THOR.RUNE", rawPosition.runeWithdrawn),
+    poolShare: new SwapKitNumber(rawPosition.sharedUnits).div(rawPosition.poolUnits),
+    dateLastAdded: rawPosition.dateLastAdded,
+    dateFirstAdded: rawPosition.dateFirstAdded,
+  }));
 }
