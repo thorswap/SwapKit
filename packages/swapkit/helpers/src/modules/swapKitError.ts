7476f17989759c1224fe5a201868a77583710fe9
chillios-dev <chillios@duck.com>
2024-07-02 16:26:12 +0200

    feat: add core impl for signing

--- packages/swapkit/helpers/src/modules/swapKitError.ts
+++ packages/swapkit/helpers/src/modules/swapKitError.ts
@@ -10,8 +10,11 @@ const errorCodes = {
   core_plugin_swap_not_found: 10007,
   core_approve_asset_target_invalid: 10008,
   core_explorer_unsupported_chain: 10009,
+  core_verify_message_not_supported: 10010,
   core_chain_halted: 10099,
-
+  /**
+   * Core - Wallet
+   */
   core_wallet_connection_not_found: 10100,
   core_wallet_xdefi_not_installed: 10101,
   core_wallet_evmwallet_not_installed: 10102,
@@ -22,6 +25,7 @@ const errorCodes = {
   core_wallet_keplr_not_installed: 10107,
   core_wallet_okx_not_installed: 10108,
   core_wallet_keepkey_not_installed: 10109,
+  core_wallet_sign_message_not_supported: 10110,
   /**
    * Core - Swap
    */
@@ -103,7 +107,6 @@ const errorCodes = {
    * SwapKit API
    */
   api_v2_invalid_response: 50001,
-
   /**
    * Toolboxes
    */
