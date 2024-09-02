59447e00ea8f32e30fbd61b9aa6e07314b42d6b6
towan <95243956+towanTG@users.noreply.github.com>
2024-09-02 16:19:17 +0400

    fix: assetValue set for synths (#970)

---
+++ .changeset/new-walls-hope.md
@@ -0,0 +1,5 @@
+---
+"@swapkit/helpers": patch
+---
+
+Fixes AssetValue set for synths
--- packages/wallets/coinbase/src/index.ts
+++ packages/wallets/coinbase/src/index.ts
@@ -1,4 +1,4 @@
-import type { CoinbaseWalletSDKOptions } from "@coinbase/wallet-sdk/dist/CoinbaseWalletSDK.js";
+import type { CoinbaseWalletSDKOptions } from "@coinbase/wallet-sdk/dist/CoinbaseWalletSDK";
 import { setRequestClientConfig } from "@swapkit/helpers";
 import { Chain, type ConnectWalletParams, WalletOption } from "@swapkit/helpers";
 
