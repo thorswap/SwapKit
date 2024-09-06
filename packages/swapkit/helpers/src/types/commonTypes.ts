f081949efe7ce04a55ec5256cd5a8161bfbbf388
towan <towan@thorswap.finance>
2024-09-06 13:13:10 +0400

    fix: radixWallet

--- packages/swapkit/helpers/src/types/commonTypes.ts
+++ packages/swapkit/helpers/src/types/commonTypes.ts
@@ -47,6 +47,14 @@ export type ConnectConfig = {
    * @optional for setting the chainflip broker url
    */
   chainflipBrokerUrl?: string;
+  /**
+   * @optional for setting the radix wallet
+   */
+  radixDappConfig?: {
+    dAppDefinitionAddress: string;
+    applicationName: string;
+    applicationVersion: string;
+  };
 };
 
 export type ConnectWalletParams<M = { [key in string]: any }> = {
