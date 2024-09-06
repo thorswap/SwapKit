80a62e1c8e237940f7e824e23fe552c4fc190739
towan <towan@thorswap.finance>
2024-09-06 12:34:58 +0400

    fix: radixWallet

--- packages/wallets/radix/src/radixWallet.ts
+++ packages/wallets/radix/src/radixWallet.ts
@@ -1,31 +1,46 @@
+import {
+  type FungibleResourcesCollectionItem,
+  GatewayApiClient,
+  type StateEntityDetailsVaultResponseItem,
+  type StateEntityFungiblesPageRequest,
+  type StateEntityFungiblesPageResponse,
+} from "@radixdlt/babylon-gateway-api-sdk";
 import { DataRequestBuilder, RadixDappToolkit } from "@radixdlt/radix-dapp-toolkit";
 import {
+  AssetValue,
   Chain,
   type ConnectWalletParams,
-  RPCUrl,
   WalletOption,
   setRequestClientConfig,
 } from "@swapkit/helpers";
-import { RadixMainnet, type RadixSigner } from "@swapkit/toolbox-radix";
+
+const RadixMainnet = {
+  networkId: 1,
+  networkName: "mainnet",
+  dashboardBase: "https://dashboard.radixdlt.com",
+};
+
+type RadixDappConfig = {
+  dAppDefinitionAddress: string;
+  network: typeof RadixMainnet;
+  applicationName: string;
+  applicationVersion: string;
+};
 
 // TODO figure out way to make wallet work nicely with toolbox without reimplementing all the methods
-const RadixSignerInstance = (): RadixSigner & { getAddress: () => Promise<string> } => {
-  const rdt = RadixDappToolkit({
-    dAppDefinitionAddress: "account_rdx128r289p58222hcvev7frs6kue76pl7pdcnw8725aw658v0zggkh9ws",
-    networkId: RadixMainnet.networkId,
-    applicationName: "Swapkit Playground",
-    applicationVersion: "1.0.0",
-  });
-
+const RadixSignerInstance = (
+  rdt: RadixDappToolkit,
+): Todo & { getAddress: () => Promise<string> } => {
   return {
-    getAddress: () => {
+    getAddress: async () => {
       return new Promise((resolve) => {
         const existingWalletData = rdt.walletApi.getWalletData();
         const account = existingWalletData?.accounts?.[0];
 
+        rdt.walletApi.setRequestData(DataRequestBuilder.accounts().exactly(1));
+
         if (account) resolve(account.address);
 
-        rdt.walletApi.setRequestData(DataRequestBuilder.accounts().atLeast(1));
         rdt.walletApi.sendRequest();
 
         rdt.walletApi.walletData$.subscribe((state) => {
@@ -56,37 +71,189 @@ const RadixSignerInstance = (): RadixSigner & { getAddress: () => Promise<string
   };
 };
 
-const getWalletMethods = async () => {
-  const { getRadixCoreApiClient, RadixToolbox, RadixMainnet } = await import(
-    "@swapkit/toolbox-radix"
-  );
+async function fetchFungibleResources({
+  address,
+  networkApi,
+}: Todo): Promise<FungibleResourcesCollectionItem[]> {
+  let hasNextPage = true;
+  let nextCursor = undefined;
+  let fungibleResources: FungibleResourcesCollectionItem[] = [];
+  const stateVersion = await currentStateVersion(networkApi);
+  while (hasNextPage) {
+    const stateEntityFungiblesPageRequest: StateEntityFungiblesPageRequest = {
+      address: address,
+      limit_per_page: 100,
+      cursor: nextCursor,
+      at_ledger_state: {
+        state_version: stateVersion,
+      },
+    };
 
-  const api = await getRadixCoreApiClient(RPCUrl.Radix, RadixMainnet);
+    const stateEntityFungiblesPageResponse: StateEntityFungiblesPageResponse =
+      await networkApi.state.innerClient.entityFungiblesPage({
+        stateEntityFungiblesPageRequest: stateEntityFungiblesPageRequest,
+      });
 
-  const signer = await RadixSignerInstance();
-  const toolbox = await RadixToolbox({ api, signer });
+    fungibleResources = fungibleResources.concat(stateEntityFungiblesPageResponse.items);
+    if (stateEntityFungiblesPageResponse.next_cursor) {
+      nextCursor = stateEntityFungiblesPageResponse.next_cursor;
+    } else {
+      hasNextPage = false;
+    }
+  }
+  return fungibleResources;
+}
 
-  const address = await signer.getAddress();
+async function currentStateVersion(networkApi: GatewayApiClient) {
+  return networkApi.status.getCurrent().then((status) => status.ledger_state.state_version);
+}
+
+function getBalance({ networkApi }: { networkApi: GatewayApiClient }) {
+  return async function getBalance(address: string) {
+    const fungibleResources = await fetchFungibleResources({ address, networkApi });
+    const fungibleBalances = convertResourcesToBalances({
+      resources: fungibleResources,
+      networkApi,
+    });
+    return fungibleBalances;
+  };
+}
+
+// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: <explanation>
+async function convertResourcesToBalances({
+  resources,
+  networkApi,
+}: {
+  resources: FungibleResourcesCollectionItem[]; //| NonFungibleResourcesCollectionItem[];
+  networkApi: GatewayApiClient;
+}): Promise<AssetValue[]> {
+  const balances: AssetValue[] = [];
+  const BATCH_SIZE = 50;
+
+  // Split resources into batches of up to 50 items
+  const resourceBatches = [];
+  for (let i = 0; i < resources.length; i += BATCH_SIZE) {
+    resourceBatches.push(resources.slice(i, i + BATCH_SIZE));
+  }
+
+  for (const batch of resourceBatches) {
+    const addresses = batch.map((item) => item.resource_address);
+    const response: StateEntityDetailsVaultResponseItem[] =
+      await networkApi.state.getEntityDetailsVaultAggregated(addresses);
+
+    const divisibilities = new Map<string, { decimals: number; symbol: string }>();
+
+    for (const result of response) {
+      if (result.details !== undefined) {
+        const metaDataSymbol = result.metadata?.items.find((item) => item.key === "symbol");
+        const symbol =
+          metaDataSymbol?.value.typed.type === "String" ? metaDataSymbol.value.typed.value : "?";
+
+        if (result.details.type === "FungibleResource") {
+          divisibilities.set(result.address, {
+            decimals: result.details.divisibility,
+            symbol,
+          });
+        }
+      }
+    }
+
+    for (const item of batch) {
+      if (item.aggregation_level === "Global") {
+        const assetInfo = divisibilities.get(item.resource_address) || { decimals: 0, symbol: "?" };
+
+        const balance = AssetValue.from({
+          asset:
+            assetInfo.symbol !== Chain.Radix
+              ? `${Chain.Radix}.${assetInfo.symbol}-${item.resource_address}`
+              : "XRD.XRD",
+          value: item.amount,
+        });
+        balances.push(balance);
+      }
+    }
+  }
+  // Iterate through resources
+  return balances;
+}
+
+const getWalletMethods = async (dappConfig: RadixDappConfig) => {
+  const rdt = RadixDappToolkit({ ...dappConfig, networkId: dappConfig.network.networkId });
+
+  rdt.buttonApi.setTheme("black");
+
+  const signer = await RadixSignerInstance(rdt);
+
+  // const connectButtonModule = ConnectButtonModule({
+  //     networkId: 1,
+  //     onConnect: (done: (input?: {
+  //         challenge: string;
+  //     }) => void) => console.log,
+  //     providers: {
+  //         stateModule: rdt.buttonApi,
+  //         gatewayModule: GatewayModule;
+  //         walletRequestModule: WalletRequestModule;
+  //         storageModule: StorageModule<{
+  //             status: ConnectButtonStatus;
+  //         }>
+  //     }
+  // })
+
+  const networkApi = GatewayApiClient.initialize({
+    networkId: RadixMainnet.networkId,
+    applicationName: dappConfig.applicationName,
+  });
+
+  const address = (await signer.getAddress()) as string;
 
   return {
+    radixDappToolkit: rdt,
     address,
-    walletMethods: {
-      ...toolbox,
-      getAddress: signer.getAddress,
-      getBalance: () => toolbox.getBalance(address),
+    getBalance: () => getBalance({ networkApi })(address),
+    transfer: (_params: { assetValue: AssetValue; recipient: string; from: string }) => {
+      throw new Error("Not implemented");
     },
+    signAndBroadcast: async ({ manifest, message }: { manifest: string; message: string }) => {
+      const txResult = (
+        await rdt.walletApi.sendTransaction({
+          transactionManifest: manifest,
+          message,
+        })
+      ).unwrapOr(null)?.transactionIntentHash;
+
+      if (!txResult) {
+        throw new Error("wallet_radix_transaction_failed");
+      }
+
+      return txResult;
+    },
+    getAddress: signer.getAddress,
   };
 };
 
-function connectRadixWallet({ addChain, config: { thorswapApiKey } }: ConnectWalletParams) {
+function connectRadixWallet({
+  addChain,
+  config: {
+    thorswapApiKey,
+    radixDappConfig = {
+      dAppDefinitionAddress: "account_rdx128r289p58222hcvev7frs6kue76pl7pdcnw8725aw658v0zggkh9ws",
+      applicationName: "Swapkit Playground",
+      applicationVersion: "0.0.1",
+    },
+  },
+}: ConnectWalletParams & {
+  radixDappConfig?: RadixDappConfig;
+}) {
   return async function connectRadixWallet(_chains: Chain.Radix[]) {
     setRequestClientConfig({ apiKey: thorswapApiKey });
 
-    const { address, walletMethods } = await getWalletMethods();
+    const walletMethods = await getWalletMethods({
+      network: RadixMainnet,
+      ...radixDappConfig,
+    });
 
     addChain({
       chain: Chain.Radix,
-      address,
       balance: [],
       walletType: WalletOption.RADIX_WALLET,
       ...walletMethods,
